5afdd4b9c13a3cc436342e046898185aee8d219a
grouzen <grouzen.hexy@gmail.com>
2010-06-16 02:33:44 +0300

    Removed all crap of the options parsing.

--- src/commands.c
+++ src/commands.c
@@ -10,9 +10,6 @@
 #include "../include/commands.h"
 
 #define COMMAND_MAX_LENGTH 1024
-#define COMMAND_MAX_ARG_LENGTH 64
-#define COMMAND_ARG_ARG1 1
-#define COMMAND_ARG_REST 0
 
 static int command_get_type(gchar *c)
 {
@@ -41,87 +38,6 @@ static int command_get_type(gchar *c)
         type = COMMAND_TYPE_NONE;
     
     return type;
-}
-
-#if 0
-static gchar **command_get_arg(gchar *c, gint which)
-{
-    GArray *args;
-    gchar **ret;
-    gchar arg[COMMAND_MAX_ARG_LENGTH];
-    gint i = 0, j = 0, argn = which;
-
-    /* Skipping spaces on left. */
-    while(c[i++] == ' ');
-    c = &c[i - 1];
-
-    args = g_array_new(FALSE, FALSE, sizeof(gchar *));
-
-    i = 0;
-    while(1) {
-        if(c[i] == ' ' || c[i] == '\n' || c[i] == '\0') {
-            if(j) {
-                gchar *args_arg;
-                
-                arg[j++] = '\0';
-                args_arg = g_strdup(arg);
-                g_array_append_val(args, args_arg);
-                
-                if(which == COMMAND_ARG_ARG1)
-                    break;
-                j = 0;
-            }
-
-            if(c[i] == '\0')
-                break;
-            argn = 1;
-        } else {
-            if(argn > 0) {
-                if(j > COMMAND_MAX_ARG_LENGTH)
-                    j = 0;
-                arg[j++] = c[i];
-            }
-        }
-        i++;
-    }
-
-    if(args->len) {
-        gchar **ret = g_malloc0(sizeof(gchar*) * (args->len + 1));
-
-        for(i = 0; i < args->len; i++) {
-            ret[i] = g_strdup(g_array_index(args, gchar*, i));
-        }
-
-        /* The argv[] must be null terminated. */
-        if(which == COMMAND_ARG_REST)
-            ret[i] = NULL;
-    } else {
-        ret = NULL;
-    }
-
-    g_array_free(args, TRUE);
-
-    return ret;
-}
-#endif
-
-static gchar *command_usage(gint type)
-{
-    gchar *usage;
-    
-    switch(type) {
-    case COMMAND_TYPE_EXEC:
-        usage = ":e command [arg1 [arg2 [... argn]]]";
-        break;
-    case COMMAND_TYPE_MESSAGE:
-        usage = ":m jid message";
-        break;
-    default:
-        usage = "";
-        break;
-    }
-
-    return usage;
 }
 
 static gboolean command_exec(Xinb *x, gchar *command)
--- src/xinb.c
+++ src/xinb.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <unistd.h>
-#include <getopt.h>
 #include <errno.h>
 #include <string.h>
 #include <loudmouth/loudmouth.h>
@@ -15,23 +14,13 @@
 #define XINB_CONFIG_GROUP "xinb"
 
 Xinb *xinb = NULL;
-static struct option long_opts[] = {
-    {"server", 1, NULL, 's'},
-    {"username", 1, NULL, 'u'},
-    {"password", 1, NULL, 'p'},
-    {"resource", 1, NULL, 'r'},
-    {"owner", 1, NULL, 'o'},
-    {"verbose", 0, NULL, 'v'},
-    {"daemon", 0, NULL, 'd'},
-    {0, 0, 0, 0}
-};
 
 void xinb_release(Xinb *x)
 {
     if(x != NULL) {
         if(x->conn &&
            !lm_connection_close(x->conn, &(x->gerror))) {
-            g_printerr("Couldn't close connection: %s.\n",
+            g_printerr("Couldn't close connection: %s\n",
                        x->gerror->message);
             g_free(x->gerror->message);
         }
@@ -66,7 +55,7 @@ static gboolean xinb_read_config(Xinb *x)
     config_file = g_strdup_printf("%s/%s", getenv("HOME"), XINB_CONFIG_PATH);
     if(!g_key_file_load_from_file(key_file, config_file,
                                   G_KEY_FILE_NONE, &(x->gerror))) {
-        g_printerr("Couldn't load config file '%s': %s.\n",
+        g_printerr("Couldn't load config file '%s': %s\n",
                    config_file, x->gerror->message);
         ret = FALSE;
         g_free(x->gerror->message);
@@ -74,7 +63,7 @@ static gboolean xinb_read_config(Xinb *x)
     }
 
     if(!g_key_file_has_group(key_file, XINB_CONFIG_GROUP)) {
-        g_printerr("Group '%s' not exists in config file '%s'.\n",
+        g_printerr("Group '%s' not exists in config file '%s'\n",
                    XINB_CONFIG_GROUP, config_file);
         ret = FALSE;
         g_free(x->gerror->message);
@@ -87,15 +76,11 @@ static gboolean xinb_read_config(Xinb *x)
         val = g_key_file_get_value(key_file, XINB_CONFIG_GROUP,
                                    keys[i], &(x->gerror));
         if(val == NULL) {
-            g_printerr("Couldn't get value from key '%s': %s.\n",
+            g_printerr("Couldn't get value from key '%s': %s\n",
                        keys[i], x->gerror->message);
             g_free(x->gerror->message);
         }
-
-        if(!g_hash_table_lookup(x->config, keys[i])) {
-            g_hash_table_insert(x->config, g_strdup(keys[i]), g_strdup(val));
-        }
-        
+        g_hash_table_insert(x->config, g_strdup(keys[i]), g_strdup(val));
         i++;
     }
     
@@ -119,53 +104,22 @@ void signals_handler(int signum)
 
 int main(int argc, char *argv[])
 {
-    gint opt;
-    gint long_opts_len = 0;
 
+    if(argc > 2) {
+        g_print("Usage: %s [-|file]\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+    
     signal(SIGINT, signals_handler);
     
     xinb = g_malloc0(sizeof(Xinb));
     xinb->config = g_hash_table_new_full(g_str_hash, g_str_equal, NULL, g_free);
-
-    /* What I suppose to do with this shit? */
-    while((opt = getopt_long_only(argc, argv, "", long_opts, NULL)) != -1) {
-        switch(opt) {
-        case 's':
-            g_hash_table_insert(xinb->config, "server", g_strdup(optarg));
-            break;
-        case 'u':
-            g_hash_table_insert(xinb->config, "username", g_strdup(optarg));
-            break;
-        case 'p':
-            g_hash_table_insert(xinb->config, "password", g_strdup(optarg));
-            break;
-        case 'r':
-            g_hash_table_insert(xinb->config, "resource", g_strdup(optarg));
-            break;
-        case 'o':
-            g_hash_table_insert(xinb->config, "owner", g_strdup(optarg));
-            break;
-        case 'v':
-            g_hash_table_insert(xinb->config, "verbose", g_strdup("on"));
-            break;
-        case 'd':
-            g_hash_table_insert(xinb->config, "daemon", g_strdup("on"));
-            break;
-        default:
-            xinb_release(xinb);
-            exit(EXIT_FAILURE);
-        }
-        long_opts_len++;
-    }
     
     if(!xinb_read_config(xinb)) {
-        log_record(xinb, LOGS_ERR, "Config was not loaded");
+        g_printerr("Config was not loaded\n");
+        xinb_release(xinb);
+        exit(EXIT_FAILURE);
     }
-
-    if(!g_hash_table_lookup(xinb->config, "verbose"))
-        g_hash_table_insert(xinb->config, "verbose", "off");
-    if(!g_hash_table_lookup(xinb->config, "daemon"))
-        g_hash_table_insert(xinb->config, "daemon", "off");
     
     xinb->logfd = log_open(xinb);
     if(!xinb->logfd) {
@@ -196,9 +150,9 @@ int main(int argc, char *argv[])
     }
 
     /* For sending messages from stdin if it's present. */
-    if(argc - 1 > long_opts_len * 2) {
+    if(argc > 1) {
         FILE *stream;
-        gchar *input = argv[argc - 1];
+        gchar *input = argv[1];
 
         stream = g_strcmp0(input, "-") ?
             fopen(input, "r") : stdin;
